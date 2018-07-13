b37b38d702dc6ee03d562e2840d02a49df38e90f
Krupin Ivan <igelfiend@protonmail.com>
2018-07-13 11:31:52 +0700

    Translated output and comments, readme updated

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 This project demonstrates example for working with JSON
 
 At the task, we have to get data from "https://jsonplaceholder.typicode.com/todos" and make some operations with it:
-1. Count users;
-2. Count unique tasks for each user;
-3. Count completed unique tasks for each user
+1. Save response to file;
+2. Count users;
+3. Count unique tasks for each user;
+4. Count completed unique tasks for each user
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import json
 import requests
 
 def isTaskUnique(srcData, checkObj):
-    # В исходном списке в случае уникальности должен быть обнаружен только 1 элемент
-    # Если элементов больше - он уже не уникален
+    # In source list at unique case must be 1 element found
+    # In other cases, element not unique
     counter = 0
     for element in srcData:
         if element[ "title" ] == checkObj["title"]:
@@ -14,39 +14,38 @@ def isTaskUnique(srcData, checkObj):
     return True
 
 def main():
-    # считываем ответ с сайта
+    # Reading data from site
     response = requests.get( "https://jsonplaceholder.typicode.com/todos" )
     data = json.loads( response.text )
 
-    # сохраняем в файл
+    # Saving response to file
     with open( "response.txt", "w" ) as f:
         json.dump( data, f, indent=4 )
 
-    # Сформируем структуру, в которой в каждом элементе будет храниться информация о количестве
-    # уникальных задач и количестве завершенных уникальных задач
+	# Making dict with data for each user, contains unique tasks count and complete tasks count
     users = {}
 
     for record in data:
-        # Если такого пользователя нет, создать новую запись для него
+        # If no user found, make blank record for him
         if record[ "userId" ] not in users.keys():
             users[ record[ "userId" ] ] = {}
             users[ record[ "userId" ] ][ "complete_tasks" ] = 0
             users[ record[ "userId" ] ][ "unique_tasks"   ] = 0
 
-        # Проверяем на уникальность задачи
+        # Checking unique condition
         if isTaskUnique( data, record ):
             users[ record[ "userId" ] ][ "unique_tasks" ] += 1
-            # Проверяем равезрешнность задачи
+            # Checking completing condition
             if record[ "completed" ] == True:
                 users[ record[ "userId" ] ][ "complete_tasks" ] += 1
 
     for user in users:
         print( "\n" )
-        print( "Текущий пользователь: ", user )
-        print( "Оригинальных задач:   ", users[ user ][ "unique_tasks" ] )
-        print( "Выполнено задач:      ", users[ user ][ "complete_tasks" ] )
+        print( "Current user:   ", user )
+        print( "Unique tasks:   ", users[ user ][ "unique_tasks" ] )
+        print( "Complete tasts: ", users[ user ][ "complete_tasks" ] )
 
-    print( "\nКоличество уникальных пользователей: ", len( users.keys() ) )
+    print( "\nUnique users count: ", len( users.keys() ) )
 
 if __name__ == "__main__":
     main()
